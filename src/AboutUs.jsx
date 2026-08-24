d7e50ff8ed7d196453aae2ea02ea66f592a735ff
Usman Abdullah Jaral <jaralusman888@gmail.com>
2026-08-24 18:04:15 +0500

    Add AboutUs component

---
+++ src/AboutUs.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is a plant shop dedicated to bringing beautiful and
+        healthy plants into your home.
+      </p>
+      <p>
+        We offer a variety of indoor and outdoor plants to create a peaceful
+        and refreshing environment.
+      </p>
+      <p>
+        Our goal is to make plant shopping simple, affordable, and enjoyable
+        for everyone.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
